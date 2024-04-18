1f9d1346c81560d5915b336766b489ac9fee0541
faritg1 <juanagelves@gmail.com>
2024-04-17 22:50:53 -0500

    feat: :sparkles: Implementando la base de datos mediante Dbfirst

---
+++ Persistence/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence.Entities;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Code { get; set; }
+
+    public string Name { get; set; }
+
+    public float Price { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual Category Category { get; set; }
+}
