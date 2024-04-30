685bac90b4069970352072f2b2e3a7b3655a1f10
faritg1 <juanagelves@gmail.com>
2024-04-29 22:31:16 -0500

    feat: :sparkles: Implementando Dtos y Mappper

---
+++ Api/Dtos/ProductDto.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public float Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
---
+++ Api/Profiles/MappingProfiles.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Api.Dtos;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Api.Profiles
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles(){
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
+    }
+}
