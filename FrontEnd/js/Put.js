78230783827450e7d45af5b6a947dd75cadd7155
faritg1 <juanagelves@gmail.com>
2024-05-12 22:37:29 -0500

    feat: :recycle: Implementando validaciones vacias

--- FrontEnd/js/Put.js
+++ FrontEnd/js/Put.js
@@ -3,27 +3,31 @@ const Update = async() => {
     const nameUp = document.getElementById('nameUp').value;
     const createUp = document.getElementById('createUp').value;
     const updateUp = document.getElementById('updateUp').value;
+    if(!((nameUp === null) || (nameUp === undefined) || (nameUp === ""))){
+        try {
+            const url = `http://localhost:5087/Category/${storageId}`;
 
-    const url = `http://localhost:5087/Category/${storageId}`;
-
-    let newUser = {
-        name: nameUp,
-        create: createUp,
-        update: updateUp
-    };
-    console.log(newUser);
-    const options = {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newUser)
-    };
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        return result
-    } catch (error) {
-        return (error);
+            let newUser = {
+                name: nameUp,
+                create: createUp,
+                update: updateUp
+            };
+            console.log(newUser);
+            const options = {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
+            };
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result);
+            return window.location.href = "./Get.html";
+        } catch (error) {
+            return (error);
+        }
+    }else{
+        alert("Hay un campo vacio!!!");
     }
 } 
--- FrontEnd/js/post.js
+++ FrontEnd/js/post.js
@@ -2,28 +2,30 @@ const Post = async() => {
     const name = document.getElementById('nameRegister').value;
     const create = document.getElementById('createRegister').value;
     const update = document.getElementById('updateRegister').value;
-    
-    let newUser = {
-        name: name,
-        create: create,
-        update: update
-    };
-    
-    const url = "http://localhost:5087/Category";
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newUser)
-    };
-
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error(error);
-        return null;
+    if(!((name === null) || (name === undefined) || (name === ""))){
+        try {
+            let newUser = {
+                name: name,
+                create: create,
+                update: update
+            };
+            
+            const url = "http://localhost:5087/Category";
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newUser)
+            };
+            const response = await fetch(url, options);
+            const result = await response.json();
+            console.log(result);
+            return window.location.href = "./Get.html";
+        } catch (error) {
+            return "Hay un error" + error;
+        }
+    }else{
+        alert("Hay un campo vacio!!!");
     }
 } 
